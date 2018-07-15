2fca30ec4ec1b36800342a2c5a73d5eaac4bfe1c
Suhas <suhaspr01@gmail.com>
2018-07-15 16:30:25 +0530

    Update 1.0.1

--- www/EncryptedExoplayer.js
+++ www/EncryptedExoplayer.js
@@ -9,7 +9,7 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'EncryptedExoplayer';
 
 var EncryptedExoplayer = {
-  encrypt: function (filePath, key, success, error) {
+  encrypt: function (filePath, outputPath, key, success, error) {
     exec(success, error, PLUGIN_NAME, 'encrypt', [filePath, outputPath, key]);
   },
   play: function (filePath, key, success, error) {
